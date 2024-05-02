aecedc0b262134325bf36f65b93db18c21bab1bb
oliviamarsha202 <149671731+oliviamarsha202@users.noreply.github.com>
2024-05-02 21:32:28 +0700

    Update Tugas_Graph_kel9_MI23F.py

--- Tugas_Graph_kel9_MI23F.py
+++ Tugas_Graph_kel9_MI23F.py
@@ -44,3 +44,48 @@ class Peta:
         # cek apakah kedua kota ada pada daftar kota atau tidak
         if kota1 in self.daftarkota and kota2 in self.daftarkota:
             # jika ada, maka hapus kota 1 pada list kota 2 dan sebaliknya
+               self.daftarkota[kota2].remove(kota1)
+            self.daftarkota[kota1].remove(kota2)
+            return True
+        return False
+
+    # membuat fungsi untuk mengeprint daftar kota dan kota yang memiliki akses jalan ke kota lain
+    def print(self):
+        print('Berikut merupakan daftar kota yang terdaftar pada sistem peta:')
+        # melakukan iterasi pada daftarkota untuk mendapatkan daftar kota yang telah terdaftar
+        for kota in self.daftarkota:
+            # print nama kota
+            print("-", kota)
+            # cek apakah list jalan kota kosong atau tidak
+            if self.daftarkota[kota]:
+                # jika list jalan terdapat nama kota, maka print :
+                print("  Akses ke kota lain:")
+                # dan iterasi list jalan yang ada pada kota dan print jalan aksesnya
+                for jalan in self.daftarkota[kota]:
+                    print("  *", jalan)
+            # dan jika list kosong, maka print :
+            else:
+                print("  Tidak memiliki akses ke kota lain")
+
+
+
+
+# membuat list peta
+PetaJepang = Peta()
+
+# tambahkan kota
+PetaJepang.tambahkanKota("Ueno")
+PetaJepang.tambahkanKota("Shimonita")
+PetaJepang.tambahkanKota("Annaka")
+PetaJepang.tambahkanKota("Takasaki")
+PetaJepang.tambahkanKota("Ogano")
+PetaJepang.tambahkanKota("Chichibu")
+PetaJepang.tambahkanKota("Minano")
+PetaJepang.tambahkanKota("Nagatoro")
+PetaJepang.tambahkanKota("Yorii")
+PetaJepang.tambahkanKota("Honjo")
+PetaJepang.tambahkanKota("Isesaki")
+PetaJepang.tambahkanKota("Maebashi")
+
+# tambahkan jalan
+PetaJepang.tambahkanJalan("Shimonita","Annaka")
